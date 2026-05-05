06741be8295bcc25bb005cb3816c878333574680
ranan <2194672703@qq.com>
2026-05-05 12:21:18 +0800

    move xv6 to WSL native fs

--- testwrite.c
+++ testwrite.c
@@ -5,9 +5,6 @@
 int
 main(int argc, char *argv[])
 {
-  int i;
-
-  for(i = 1; i < argc; i++)
-    printf(1, "%s%s", argv[i], i+1 < argc ? " " : "\n");
+  printf(1, "hello testwrite\n");
   exit();
 }
